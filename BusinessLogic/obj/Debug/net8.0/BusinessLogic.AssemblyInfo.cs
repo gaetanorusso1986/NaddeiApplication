54c7a7d9019d0c0c9475720c0d5921a931731e22
Domenico Naddei <D.Naddei@almaviva.it>
2025-03-24 14:37:11 +0100

    .

--- BusinessLogic/obj/Debug/net8.0/BusinessLogic.AssemblyInfo.cs
+++ BusinessLogic/obj/Debug/net8.0/BusinessLogic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLogic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62ba6b283384f2a0d44de8d6bfccb9e049d230b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfc577c082adf5715160b9e5cbd802c8f95e008b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLogic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLogic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
